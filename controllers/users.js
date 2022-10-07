094f6f17068304ae8853d79827aa6e8282593a02
touchresponce <touchresponce71@gmail.com>
2022-10-07 03:19:09 +0300

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -74,21 +74,16 @@ module.exports.updateAvatar = (req, res, next) => {
     { avatar },
     { new: true, runValidators: true }
   )
-    .orFail(() => {
-      throw new NotFound("Пользователь с указанным _id не найден");
-    })
     .then((user) => res.send(user))
     .catch((err) => {
       if (err.name === "ValidationError" || err.name === "CastError") {
-        throw new BadRequest(
-          "Переданы некорректные данные при обновлении аватара"
-        );
+        throw new BadRequest("Переданы некорректные данные");
       }
     })
     .catch(next);
 };
 
-// обновить определенного
+// обновить текущего
 module.exports.updateUser = (req, res, next) => {
   const { name, about } = req.body;
 
@@ -103,9 +98,7 @@ module.exports.updateUser = (req, res, next) => {
     .then((user) => res.send(user))
     .catch((err) => {
       if (err.name === "ValidationError" || err.name === "CastError") {
-        throw new BadRequest(
-          "Переданы некорректные данные при обновлении профиля"
-        );
+        throw new BadRequest("Переданы некорректные данные");
       }
     })
     .catch(next);
